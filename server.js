641d417ac26fe134377686656b53f8b892066f37
FThelin <fredrikthelin82@gmail.com>
2023-02-27 11:57:15 +0100

    close server

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
   res.status(200).send("HELLO, I´M DEPLOYED ON VERCEL!!");
 });
 
-app.listen(process.env.PORT || 3000, () =>
+const server = app.listen(process.env.PORT || 3000, () =>
   console.log("Server is up and running")
 );
+
+server.close();
